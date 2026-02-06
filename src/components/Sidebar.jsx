b27264cc02d41e832d83c4249abda85d9765e79e
isiid7783 <llcterraio@gmail.com>
2026-02-06 23:43:11 +0900

    Create Sidebar.jsx

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h1>Plato</h1>
+      <div className="menu">
+        <div>Chats</div>
+        <div>Friends</div>
+        <div>Groups</div>
+        <div>Settings</div>
+      </div>
+    </div>
+  )
+}
