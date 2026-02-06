d3149d6c8aa43807789e5fe46d8869a3143d0d81
isiid7783 <llcterraio@gmail.com>
2026-02-06 23:57:34 +0900

    Create messageStore.js

---
+++ src/messageStore.js
@@ -0,0 +1,8 @@
+export function saveMessages(chat, messages) {
+  localStorage.setItem("chat_" + chat, JSON.stringify(messages))
+}
+
+export function loadMessages(chat) {
+  const data = localStorage.getItem("chat_" + chat)
+  return data ? JSON.parse(data) : []
+}
