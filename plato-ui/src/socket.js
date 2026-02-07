f837b0cec975fa3a07c7fefe48b3d4ec0564e00a
isiid7783 <llcterraio@gmail.com>
2026-02-07 19:54:52 +0900

    Add files via upload

---
+++ plato-ui/src/socket.js
@@ -0,0 +1,17 @@
+export function createSocket(user, onMessage) {
+  const ws = new WebSocket("ws://localhost:8080")
+
+  ws.onopen = () => {
+    ws.send(JSON.stringify({
+      type: "register",
+      user
+    }))
+  }
+
+  ws.onmessage = (event) => {
+    const data = JSON.parse(event.data)
+    onMessage(data)
+  }
+
+  return ws
+}
